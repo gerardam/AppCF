a6920160f67bb3a35ab014b7e1462d532283af9b
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-08-05 21:29:55 -0500

    model,view,tamplate de categoria, listado de elementos

--- inv/models.py
+++ inv/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 from bases.models import ClaseModelo
 
+class Categoria(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
+        help_text='Descripción de la categoría',
+        unique=True
+    )
 
+    def __str__(self):
+        return '{}'.format(self.descripcion)
 
+    def Save(self):
+        self.descripcion = self.descripcion.upper()
+        super(Categoria, self).Save()
+
+    class Meta:
+        verbose_name_plural = 'Categorías'
