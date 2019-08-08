8bd7770623e9083f37fc22720272bfa9937a087a
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-08-08 13:17:09 -0500

    Personalizar color al inactivar una marca desde la base

--- inv/views.py
+++ inv/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views import generic
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Categoria, SubCategoria, Marca, UnidadMedida, Producto
 from .forms import CategoriaForm, SubCategoriaForm, MarcaForm, UMForm, ProductoForm
@@ -123,6 +124,7 @@ def marca_inactivar(request, id):
     if request.method=='POST':
         marca.estado = False
         marca.save()
+        messages.success(request, 'Marca inactivada')
         return redirect('inv:marca_list')
 
     return render(request, template_name, contexto)
