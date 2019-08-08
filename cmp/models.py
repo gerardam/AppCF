cb5776c9936f1a8a956747eccc36fb8036c75a9f
Gerardo Alvarez Mendoza <gerardam@outlook.com>
2019-08-07 21:53:37 -0500

    creacion de modelos, urls y vista

--- cmp/models.py
+++ cmp/models.py
@@ -1,28 +1,35 @@
 from django.db import models
 from bases.models import ClaseModelo
 
-class Producto(ClaseModelo):
-    codigo = models.CharField(
-        max_length=20,
+class Proveedor(ClaseModelo):
+    descripcion = models.CharField(
+        max_length=100,
         unique=True
     )
-    codigo_barra = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=200)
-    precio = models.FloatField(default=0)
-    existencia = models.IntegerField(default=0)
-    ultima_compra = models.DateField(null=True, blank=True)
-
-    marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
-    unidad_medida = models.ForeignKey(UnidadMedida, on_delete=models.CASCADE)
-    subcategoria = models.ForeignKey(SubCategoria, on_delete=models.CASCADE)
-
+    direccion = models.CharField(
+        max_length=250,
+        unique=True
+    )
+    contacto = models.CharField(
+        max_length=100
+    )
+    telefono = models.CharField(
+        max_length=10,
+        null=True,
+        blank=True
+    )
+    email = models.CharField(
+        max_length=250,
+        null=True,
+        blank=True
+    )
+    
     def __str__(self):
         return '{}'.format(self.descripcion)
 
     def Save(self):
         self.descripcion = self.descripcion.upper()
-        super(Producto, self).Save()
+        super(Proveedor, self).Save()
 
     class Meta:
-        verbose_name_plural = 'Productos'
-        unique_together = ('codigo','codigo_barra')
+        verbose_name_plural = 'Proveedores'
--- cmp/views.py
+++ cmp/views.py
@@ -1,3 +1,54 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views import generic
+from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .models import Proveedor
+from .forms import ProveedorForm
 
-# Create your views here.
+class ProveedorView(LoginRequiredMixin, generic.ListView):
+    model = Proveedor
+    template_name = 'cmp/proveedor_list.html'
+    context_object_name = 'obj'
+    login_url = 'bases:login'
+
+class ProveedorNew(LoginRequiredMixin, generic.CreateView):
+    model = Proveedor
+    template_name = 'cmp/proveedor_form.html'
+    context_object_name = 'obj'
+    form_class = ProveedorForm
+    success_url = reverse_lazy('inv:proveedor_list')
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.uc = self.request.user
+        return super().form_valid(form)
+
+class ProveedorEdit(LoginRequiredMixin, generic.UpdateView):
+    model = Proveedor
+    template_name = 'inv/proveedor_form.html'
+    context_object_name = 'obj'
+    form_class = ProveedorForm
+    success_url = reverse_lazy('inv:proveedor_list')
+    login_url = 'bases:login'
+
+    def form_valid(self, form):
+        form.instance.um = self.request.user.id
+        return super().form_valid(form)
+
+def proveedor_inactivar(request, id):
+    provee = Proveedor.objects.filter(pk=id).first()
+    contexto = {}
+    template_name = 'inv/catalogos_del.html'
+
+    if not provee:
+        return redirect('inv:proveedor_list')
+
+    if request.method=='GET':
+        contexto = {'obj':provee}
+
+    if request.method=='POST':
+        provee.estado = False
+        provee.save()
+        return redirect('inv:proveedor_list')
+
+    return render(request, template_name, contexto)
